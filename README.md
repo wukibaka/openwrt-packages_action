bd228759df8745ed849a4d441d2dd242f79b53c6
wukibaka <wq18823645769@gmail.com>
2024-06-01 20:49:41 +0800

    Some modifications

--- README.md
+++ README.md
@@ -1,37 +1,28 @@
 # openwrt-packages
-国内常用 OpenWrt 软件包源码合集，每天自动更新，建议使用 lean 源码
 
-## 由于 GitHub 会对近期未提交的仓库 Actions 功能进行冻结，如果您发现自动更新出现问题，请在 issues 提出！
+自用软件包源码合集，每天自动更新，建议使用 [immortalwrt](https://github.com/immortalwrt/immortalwrt) 源码。
 
+## 食用方式（二选一）：
 
-## 欢迎加入群聊【OP编译交流小组】：[472239598](https://jq.qq.com/?_wv=1027&k=Lzxb18xM)
-## 推广
-- [倾城极速机场](https://qcjs.cc)
-
-## 有问题请发issue，我不上恩山的！！！
-请求加入xxx软件包/问题反馈也请使用issue  
-您也可以在[这个仓库](https://github.com/liuran001/openwrt-packages_action)发送PR
-
-
-## 食用方式（三选一）：
 `还是建议按需取用，不然碰到依赖问题不太好解决`
-1. 先cd进package目录，然后执行
-```bash
- git clone https://github.com/liuran001/openwrt-packages
-```
-2. 或者添加下面代码到feeds.conf.default文件
-```bash
- src-git liuran001_packages https://github.com/liuran001/openwrt-packages
-```
-3. 先cd进package目录，然后执行
-```bash
- svn co https://github.com/liuran001/openwrt-packages/branches/packages
-```
 
-## 不要为了下载而Fork这个项目
+1. 先 `cd` 进 `package` 目录，然后执行：
+    ```bash
+    git clone https://github.com/wukibaka/openwrt-packages
+    ```
 
-## 赞助
-- [爱发电](https://afdian.net/a/BDovo)
-## Stargazers over time
+2. 或者添加下面代码到 `feeds.conf.default` 文件：
+    ```bash
+    src-git wukibaka_packages https://github.com/wukibaka/openwrt-packages
+    ```
 
-[![Stargazers over time](https://starchart.cc/liuran001/openwrt-packages.svg)](https://starchart.cc/liuran001/openwrt-packages)
+## 插件列表
+
+- [immortalwrt-packages](https://github.com/immortalwrt/packages)
+- [luci-app-adguardhome](https://github.com/kenzok78/luci-app-adguardhome)
+- [luci-app-argon-config](https://github.com/jerrykuku/luci-app-argon-config)
+- [luci-app-mosdns](https://github.com/sbwml/luci-app-mosdns)
+- [luci-app-openclash](https://github.com/vernesong/OpenClash)
+- [luci-app-smartdns](https://github.com/pymumu/luci-app-smartdns)
+- [luci-app-ssr-plus](https://github.com/fw876/helloworld)
+- [luci-theme-argon](https://github.com/jerrykuku/luci-theme-argon)
--- packages.sh
+++ packages.sh
@@ -1,114 +1,65 @@
 #!/bin/bash
-svn co https://github.com/immortalwrt/packages/trunk/net/adguardhome
-svn co https://github.com/immortalwrt/packages/trunk/net/gost
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome ./luci-app-adguardhome
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-aliddns
-svn co https://github.com/frainzy1477/luci-app-clash/trunk ./luci-app-clash
-rm -rf luci-app-openclash
-rm -rf OpenClash
-git clone https://github.com/vernesong/OpenClash.git
-mv -f OpenClash/luci-app-openclash .
-rm -rf OpenClash
-svn co https://github.com/garypang13/luci-app-eqos/trunk ./luci-app-eqos
-svn co https://github.com/jerrykuku/luci-app-jd-dailybonus/trunk ./luci-app-jd-dailybonus
-svn co https://github.com/jerrykuku/luci-theme-argon/branches/18.06 ./luci-theme-argon
-svn co https://github.com/jerrykuku/luci-app-vssr/trunk ./luci-app-vssr
-svn co https://github.com/tty228/luci-app-serverchan/trunk ./luci-app-serverchan
-rm -rf .svn
-svn co https://github.com/fw876/helloworld/trunk ./
-rm -rf .svn
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk ./
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-smartdns
-svn co https://github.com/kenzok8/openwrt-packages/trunk/smartdns
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-argon_new
-svn co https://github.com/kenzok8/luci-theme-ifit/trunk/luci-theme-ifit
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-atmaterial
-svn co https://github.com/garypang13/luci-theme-edge/branches/18.06 ./luci-theme-edge
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-opentomato
-svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-theme-opentomcat
-svn co https://github.com/jerrykuku/node-request/trunk ./node-request
-svn co https://github.com/jerrykuku/lua-maxminddb/trunk ./lua-maxminddb
-svn co https://github.com/rosywrt/luci-theme-rosy/trunk/luci-theme-rosy
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/UnblockNeteaseMusic
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/UnblockNeteaseMusicGo
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/adbyby
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/baidupcs-web
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-baidupcs-web
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-adbyby-plus
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-airplay2
-svn co https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom/trunk/luci-theme-infinityfreedom
-svn co https://github.com/immortalwrt/luci-app-koolproxyR/trunk ./luci-app-koolproxyR
-svn co https://github.com/immortalwrt/packages/trunk/net/GoQuiet
-svn co https://github.com/immortalwrt/packages/trunk/net/chinadns
-svn co https://github.com/lisaac/luci-app-dockerman/trunk/applications/luci-app-dockerman
-svn co https://github.com/lisaac/luci-lib-docker/trunk/collections/luci-lib-docker
-svn co https://github.com/lisaac/luci-app-diskman/trunk ./luci-app-diskman
-rm -rf parted
-mkdir parted
-wget https://raw.githubusercontent.com/lisaac/luci-app-diskman/master/Parted.Makefile -O parted/Makefile
-rm -rf .svn
-svn co https://github.com/destan19/OpenAppFilter/trunk ./
-svn co https://github.com/4IceG/luci-app-3ginfo/trunk/luci-app-3ginfo
-rm -rf luci-app-wrtbwmon
-rm -rf wrtbwmonluci
-git clone -b old-master https://github.com/brvphoenix/luci-app-wrtbwmon.git wrtbwmonluci
-mv -f wrtbwmonluci/luci-app-wrtbwmon .
-rm -rf wrtbwmonluci
-svn co https://github.com/riverscn/openwrt-iptvhelper/trunk/iptvhelper
-svn co https://github.com/riverscn/openwrt-iptvhelper/trunk/luci-app-iptvhelper
-svn co https://github.com/brvphoenix/wrtbwmon/branches/old-master/wrtbwmon
-svn co https://github.com/KFERMercer/luci-app-tcpdump/trunk ./luci-app-tcpdump
-svn co https://github.com/tty228/luci-app-nodogsplash/trunk ./luci-app-nodogsplash
-svn co https://github.com/koshev-msk/luci-app-atinout/trunk ./luci-app-atinout
-mkdir ./atinout
-mv ./luci-app-atinout/Makefile.atinout ./atinout/Makefile
-svn co https://github.com/muink/luci-app-pcap-dnsproxy/branches/dev ./luci-app-pcap-dnsproxy
-svn co https://github.com/koshev-msk/3proxy-openwrt/trunk ./3proxy
-svn co https://github.com/pexcn/openwrt-chinadns-ng/branches/luci ./luci-app-chinadns-ng
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-usb-printer
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ramfree
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-ttyd
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-unblockmusic
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vlmcsd
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/vlmcsd
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-vsftpd
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-xlnetacc
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-frpc
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/frp
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-frps
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_aliyun
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/ddns-scripts_dnspod
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-cifs-mount
-git clone https://github.com/yiguihai/luci-app-passwall-plus
-svn co https://github.com/immortalwrt/packages/trunk/net/udpspeeder
-git clone https://github.com/koshev-msk/luci-app-modeminfo
-svn co https://github.com/immortalwrt/packages/trunk/net/pcap-dnsproxy
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/microsocks
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/redsocks2
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/tcpping
-svn co https://github.com/immortalwrt/packages/trunk/net/dnscrypt-proxy
-svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/dnsforwarder
-svn co https://github.com/obsy/packages/trunk/3ginfo
-svn co https://github.com/liuran001/openwrt-theme/trunk/luci-theme-argon-lr
-git clone https://github.com/jerrykuku/luci-app-argon-config
-svn co https://github.com/BCYDTZ/luci-app-UUGameAcc/trunk ./luci-app-UUGameAcc
-svn co https://github.com/garypang13/openwrt-bypass/trunk/luci-app-bypass
-svn co https://github.com/garypang13/openwrt-bypass/trunk/smartdns-le
-svn co https://github.com/esirplayground/luci-app-LingTiGameAcc/trunk ./luci-app-LingTiGameAcc
-svn co https://github.com/esirplayground/LingTiGameAcc/trunk ./LingTiGameAcc
-svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/luci-app-aliyundrive-webdav
-svn co https://github.com/messense/aliyundrive-webdav/trunk/openwrt/aliyundrive-webdav
-svn co https://github.com/sbwml/luci-app-mosdns/trunk/mosdns ./mosdns
-svn co https://github.com/sbwml/luci-app-mosdns/trunk/luci-app-mosdns ./luci-app-mosdns
-git clone https://github.com/sbwml/v2ray-geodata
-git clone https://github.com/TorBoxCode/luci-app-tencentddns
-svn co https://github.com/TorBoxCode/luci-app-tencentddns/trunk/tools/po2lmo ./po2lmo
+function merge_package() {
+    # 参数1是分支名,参数2是库地址,参数3是所有文件下载到指定路径。
+    # 同一个仓库下载多个文件夹直接在后面跟文件名或路径，空格分开。
+    if [[ $# -lt 3 ]]; then
+        echo "Syntax error: [$#] [$*]" >&2
+        return 1
+    fi
+    trap 'rm -rf "$tmpdir"' EXIT
+    branch="$1" curl="$2" target_dir="$3" && shift 3
+    rootdir="$PWD"
+    localdir="$target_dir"
+    [ -d "$localdir" ] || mkdir -p "$localdir"
+    tmpdir="$(mktemp -d)" || exit 1
+    git clone -b "$branch" --single-branch --depth 1 --filter=blob:none --sparse "$curl" "$tmpdir"
+    cd "$tmpdir"
+    git sparse-checkout init --cone
+    git sparse-checkout set "$@"
+    # 使用循环逐个移动文件夹
+    for folder in "$@"; do
+        mv -f "$folder" "$rootdir/$localdir"
+    done
+    cd "$rootdir"
+}
 
-rm -rf .svn
-rm -rf ./*/.git
-rm -rf ./*/.svn 
-rm -f .gitattributes .gitignore
-rm -rf .github
-rm LICENSE
-mv LICENSE.packages LICENSE
+# adguardhome
+merge_package main https://github.com/kenzok8/wall packages adguardhome
+git clone -b main --single-branch --depth 1 --filter=blob:none https://github.com/kenzok78/luci-app-adguardhome packages/luci-app-adguardhome
+
+# argon
+git clone -b master --single-branch --depth 1 --filter=blob:none https://github.com/jerrykuku/luci-app-argon-config packages/luci-app-argon-config
+git clone -b master --single-branch --depth 1 --filter=blob:none https://github.com/jerrykuku/luci-theme-argon packages/luci-theme-argon
+
+# mosdns
+merge_package v5 https://github.com/sbwml/luci-app-mosdns packages luci-app-mosdns mosdns v2dat
+
+# openclash
+merge_package dev https://github.com/vernesong/OpenClash packages luci-app-openclash
+
+# smartdns
+merge_package master https://github.com/immortalwrt/packages packages net/smartdns
+git clone -b master --single-branch --depth 1 --filter=blob:none https://github.com/pymumu/luci-app-smartdns packages/luci-app-smartdns
+
+# ssrplus
+merge_package master https://github.com/fw876/helloworld packages luci-app-ssr-plus shadow-tls
+merge_package master https://github.com/immortalwrt/packages packages net/dns2socks net/dns2tcp devel/gn net/ipt2socks lang/lua-neturl net/microsocks net/simple-obfs net/tcping net/trojan net/tuic-client net/v2raya
+merge_package v5 https://github.com/sbwml/openwrt_helloworld packages chinadns-ng hysteria naiveproxy redsocks2 shadowsocks-rust shadowsocksr-libev v2ray-core v2ray-plugin xray-core xray-plugin
+
+# some modifications
+sed -i -e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' -e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' packages/*/Makefile
+
+# adguardhome
+rm -f ./packages/adguardhome/files/adguardhome.init
+curl -o ./packages/adguardhome/files/adguardhome.init https://raw.githubusercontent.com/immortalwrt/packages/master/net/adguardhome/files/adguardhome.init
+
+# clean up
+find . -type d -name ".git" -exec rm -rf {} +
+find . -type f -name ".gitattributes" -exec rm -f {} +
+find . -type d -name ".github" -exec rm -rf {} +
+find . -type f -name ".gitignore" -exec rm -f {} +
+find . -type f -name "LICENSE" ! -path './LICENSE' -exec rm -f {} +
+find . -type f \( -iname 'README.md' -o -iname 'README_ZH.md' -o -iname 'RELEASE.md' -o -iname 'RELEASE_ZH.md' \) ! -path './packages/luci-theme-argon/htdocs/luci-static/argon/background/README.md' ! -path './README.md' -exec rm -f {} +
+find . -type d -name "Screenshots" -exec rm -rf {} +
+
 exit 0
